4340b66bb5e8efa49c4165e9bde1eeec191b3f43
sherry0008 <137270366@qq.com>
2023-05-26 11:30:25 +0800

    RestApiDemoApplication

---
+++ sbur-rest-demo/src/main/java/com/thehecklers/sburrestdemo/RestApiDemoController.java
@@ -0,0 +1,60 @@
+package com.thehecklers.sburrestdemo;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/coffees")
+public class RestApiDemoController {
+    private List<Coffee> coffees= new ArrayList<>();
+
+    public RestApiDemoController() {
+        coffees.addAll(List.of(
+                new Coffee("Café Ganador"),
+                new Coffee("Café Lareño"),
+                new Coffee("Café Três Pontas"),
+                new Coffee("Café Cereza")));
+    }
+
+    @GetMapping
+    Iterable<Coffee> getCoffees() {
+        return coffees;
+    }
+
+    @GetMapping("/{id}")
+    Optional<Coffee> getCoffeeById(@PathVariable String id) {
+        for (Coffee c: coffees){
+            if (c.getId().equals(id)) {
+                return Optional.of(c);
+            }
+        }
+
+        return Optional.empty();
+    }
+
+    @PostMapping
+    Coffee postCoffee(@RequestBody Coffee coffee){
+        coffees.add(coffee);
+        return coffee;
+    }
+
+    @PutMapping("/{id}")
+    Coffee putCoffee(@PathVariable String id, @RequestBody Coffee coffee){
+        int coffeeIndex = -1;
+        for (Coffee c:coffees){
+            if (c.getId().equals(id)){
+                coffeeIndex = coffees.indexOf(c);
+                coffees.set(coffeeIndex, coffee);
+            }
+        }
+        return (coffeeIndex == -1)? postCoffee(coffee):coffee;
+    }
+
+    @DeleteMapping("/coffees/{id}")
+    void deleteCoffee(@PathVariable String id){
+        coffees.removeIf(c->c.getId().equals(id));
+    }
+}
